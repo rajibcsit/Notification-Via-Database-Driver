b5b59135b529cab46c4f0fa7950896cd86989759
rajibcsit1 <18192203021@cse.bubt.edu.bd>
2024-12-30 11:18:37 +0600

    Create notifications table and class

---
+++ app/Notifications/PostApproved.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class PostApproved extends Notification
+{
+    use Queueable;
+    protected $post;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toDatabase($notifiable)
+    {
+        return [
+            'post_id' => $this->post->id,
+            'title' => $this->post->title,
+            'body' => $this->post->body,
+            'message' => "Your post titled '{$this->post->title}' has been approved."
+        ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
