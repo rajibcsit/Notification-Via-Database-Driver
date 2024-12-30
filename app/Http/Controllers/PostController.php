519ee20a8a6d2064ded78c2b30fdbf1a4988fe1e
rajibcsit1 <18192203021@cse.bubt.edu.bd>
2024-12-30 12:06:17 +0600

    Create post blade file and create and show post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * Display a listing of all posts.
+     *
+     * @param Request $request
+     * @return \Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        // Fetch all posts with eager loading
+        $posts = Post::with('user')->latest()->get();
+
+        return view('posts', compact('posts'));
+    }
+
+    /**
+     * Store a newly created post in storage.
+     *
+     * @param PostRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(PostRequest $request)
+    {
+        // Create the post
+        Post::create([
+            'user_id' => auth()->id(),
+            'title' => $request->title,
+            'body' => $request->body,
+        ]);
+
+        return redirect()->back()->with('success', 'Post created successfully.');
+    }
 }
